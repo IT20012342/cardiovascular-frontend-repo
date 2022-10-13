2d7701e829950adcee3f4de711f0103c97aece71
Tinul Piumika <it20188054@my.sliit.lk>
2022-10-14 00:59:42 +0530

    change mertics

--- src/components/addDetails.js
+++ src/components/addDetails.js
@@ -169,7 +169,7 @@ export default function AddDetails() {
                   type="number"
                   class="form-control"
                   id="cholesterolLevel"
-                  placeholder="mg/dL"
+                  placeholder="mg/dl"
                   onChange={(e) => {
                     setCholesterol(e.target.value);
                   }}
